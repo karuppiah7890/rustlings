22546126f9fc87952275b81df72c550da91f98a4
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-08-07 23:38:46 +0530

    finish if 2 exercise
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -7,10 +7,10 @@
 // I AM NOT DONE
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+    match fizzish {
+        "fizz" => "foo",
+        "fuzz" => "bar",
+        _ => "baz"
     }
 }
 

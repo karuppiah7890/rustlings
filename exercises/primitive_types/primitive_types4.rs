641f07ef3e737d2283cdd2b77db1fb643b43eaaa
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-08-11 09:51:26 +0530

    finish primitive types 4
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,19 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
+// https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#the-stack-and-the-heap
+
+// https://doc.rust-lang.org/book/ch04-03-slices.html
+
+// https://doc.rust-lang.org/nomicon/coercions.html
+
 // I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..=3];
 
     assert_eq!([2, 3, 4], nice_slice)
 }

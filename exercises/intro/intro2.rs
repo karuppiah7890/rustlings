54d864950c8d612bff7e07c8f328d3cd5a63043f
Karuppiah Natarajan <karuppiah7890@gmail.com>
2023-02-13 14:43:58 +0530

    ex 2

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -5,5 +5,19 @@
 // I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!", "World");
+    // println!("Hello {}!", "World", 2); // this is wrong!
+    println!("Hello {1}, {0}", 1, 2);
+    // println!("Hello {1}, {100}", 1, 2) // this is wrong
+    // println!("Hello {1}, {100}, {0}", 1, 2, 3) // this is wrong
+
+    println!("Hello {1}, {0}, {1}", 3, 4);
+
+    println!("Hello {1}, {1}, {}", 5, 6);
+
+    println!("Hello {}, {0}, {}", 7, 8);
+
+    println!("Hello {}, {0}, {0}", 9);
+
+    // println!("Hello {}, {0}, {0}", 10, 11) this is wrong
 }
